351d0ffe5ad5cead799865c8aea1018ca9c4eda1
ghazikhalil <ghazehallakkhalel@gmail.com>
2025-09-17 19:38:27 +0300

    zlm3

--- resources/views/dashboard/roles/index.blade.php
+++ resources/views/dashboard/roles/index.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('dashboard.home') }}">الرئيسية</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">الرئيسية</a></li>
                         <li class="breadcrumb-item active">الأدوار والصلاحيات</li>
                     </ol>
                 </div>
